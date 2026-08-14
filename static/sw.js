038c1a8970b4f9e761de58dd950f9aeac6ebf058
ferxchooo <al222410839@gmail.com>
2026-08-14 13:55:40 -0600

    Add service worker with install and fetch event handlers

---
+++ static/sw.js
@@ -0,0 +1,9 @@
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Instalado');
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(fetch(e.request).catch(() => {
+        return new Response("Estás sin conexión a internet.");
+    }));
+});
